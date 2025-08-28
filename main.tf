bb3a6f3a4d0203cf299fd146c36810bc307025f1
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-28 21:21:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.22"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+  }
+}
+
 provider "google" {
   zone = var.region
   project = "ritu-pro"
 }
 
 resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.region
-
-  
-
+  name        = var.cluster_name
+  location    = var.region
   initial_node_count = var.node_count
 
   node_config {
@@ -17,4 +31,20 @@ resource "google_container_cluster" "primary" {
   }
 
   remove_default_node_pool = false
+}
+
+variable "cluster_name" {
+    default = "cluster-2"
+}
+variable "region" {
+    default = "us-central1-c"
+}
+variable "node_count" {
+    default = "2"
+}
+
+variable "node_machine_type" {
+  description = "The type of machine to use for nodes in the Kubernetes cluster"
+  type        = string
+  default     = "e2-standard-4"
 }
