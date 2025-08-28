c469aded3b468023fe3192381adb36366f5202de
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-28 18:56:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.10"
     }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9" # Use a compatible version
+    }
   }
 }
 provider "google" {
