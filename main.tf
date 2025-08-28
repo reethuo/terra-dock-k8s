98de1d6d5ec23dc21d034fa2458c0fb0a143b13f
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-28 15:52:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,24 @@ resource "google_container_cluster" "primary" {
 
   remove_default_node_pool = false
 }
+
+module "delegate" {
+  source = "harness/harness-delegate/kubernetes"
+  version = "0.2.3"
+
+  account_id = "ucHySz2jQKKWQweZdXyCog"
+  delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
+  delegate_name = "terraform-delegate-reethu"
+  deploy_mode = "KUBERNETES"
+  namespace = "harness-delegate-ng"
+  manager_endpoint = "https://app.harness.io"
+  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.08.86503"
+  replicas = 1
+  upgrader_enabled = true
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
