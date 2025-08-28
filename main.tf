02ea3faa7510e92ec31553489b1f5b29e9826c4e
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-28 15:42:55 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  zone = var.region
+  project = "ritu-pro"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+
+  
+
+  initial_node_count = var.node_count
+
+  node_config {
+    machine_type = var.node_machine_type
+    disk_size_gb = 20
+  }
+
+  remove_default_node_pool = false
+}
