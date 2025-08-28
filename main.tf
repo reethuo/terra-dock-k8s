db36a9821b44deb3859344fbfa761735e3e17cd0
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-28 21:50:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,15 +12,9 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.11"
     }
-    # Add the utils provider here
-    utils = {
-      source = "cloudposse/utils"
-      version = "~> 1.2" # Use a compatible version
-    }
-    # Add the time provider here
     time = {
       source  = "hashicorp/time"
-      version = "~> 0.9" # Use a compatible version
+      version = "~> 0.9"
     }
   }
 }
@@ -41,4 +35,45 @@ resource "google_container_cluster" "primary" {
   }
 
   remove_default_node_pool = false
+}
+
+data "google_client_config" "default" {}
+
+data "google_container_cluster" "primary_creds" {
+  name     = google_container_cluster.primary.name
+  location = google_container_cluster.primary.location
+}
+
+module "delegate" {
+  source = "harness/harness-delegate/kubernetes"
+  version = "0.2.3"
+
+  account_id = "ucHySz2jQKKWQweZdXyCog"
+  delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
+  delegate_name = "terraform-delegate-reethu"
+  deploy_mode = "KUBERNETES"
+  namespace = "harness-delegate-ng"
+  manager_endpoint = "https://app.harness.io"
+  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.08.86503"
+  replicas = 1
+  upgrader_enabled = true
+}
+
+resource "time_sleep" "wait_for_gke_cluster" {
+  create_duration = "120s"
+  depends_on = [google_container_cluster.primary]
+}
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.primary.endpoint}"
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+    token                  = data.google_client_config.default.access_token
+  }
 }
