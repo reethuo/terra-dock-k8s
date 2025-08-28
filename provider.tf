bda1d2f40a3229ed3c203a379f61162ff5ad5dbe
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-28 23:36:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,14 +12,29 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.11"
     }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.9"
-    }
-    # Add the utils provider here
-    utils = {
-      source = "cloudposse/utils"
-      version = "~> 1.2" # Use a compatible version
-    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# Get GCP client configuration for authentication
+data "google_client_config" "default" {}
+
+# Kubernetes Provider using GKE authentication
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
+
+# Helm Provider using GKE authentication
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.primary.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   }
 }
