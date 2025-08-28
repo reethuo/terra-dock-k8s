720eaf966376b345967e2281603c48208355387d
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-28 22:04:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.22"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+  }
+}
+
 provider "google" {
   zone = var.region
   project = "ritu-pro"
