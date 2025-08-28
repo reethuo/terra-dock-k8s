4089700a7e543f1b555782cf9d2269db25a78c44
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-28 22:30:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,14 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.22"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.11"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.9"
-    }
-  }
-}
-
 provider "google" {
   zone = var.region
   project = "ritu-pro"
 }
 
 resource "google_container_cluster" "primary" {
-  name        = var.cluster_name
-  location    = var.region
+  name     = var.cluster_name
+  location = var.region
+
+  
+
   initial_node_count = var.node_count
 
   node_config {
@@ -35,13 +17,6 @@ resource "google_container_cluster" "primary" {
   }
 
   remove_default_node_pool = false
-}
-
-data "google_client_config" "default" {}
-
-data "google_container_cluster" "primary_creds" {
-  name     = google_container_cluster.primary.name
-  location = google_container_cluster.primary.location
 }
 
 module "delegate" {
@@ -59,21 +34,8 @@ module "delegate" {
   upgrader_enabled = true
 }
 
-resource "time_sleep" "wait_for_gke_cluster" {
-  create_duration = "120s"
-  depends_on = [google_container_cluster.primary]
-}
-
-provider "kubernetes" {
-  host                   = "https://${google_container_cluster.primary.endpoint}"
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-  token                  = data.google_client_config.default.access_token
-}
-
 provider "helm" {
   kubernetes {
-    host                   = "https://${google_container_cluster.primary.endpoint}"
-    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-    token                  = data.google_client_config.default.access_token
+    config_path = "~/.kube/config"
   }
 }
