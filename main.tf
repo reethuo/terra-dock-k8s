9c487d99ec11df10fbdf097de374f9602739988e
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-28 18:33:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,40 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null" 
+      version = "~> 3.2" #ensures that Terraform uses version ~> 3.2
+    }
+  }
+}
 provider "google" {
-  zone = var.region
   project = "ritu-pro"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
 
-resource "google_container_cluster" "primary" {
-  name        = var.cluster_name
-  location    = var.region
-  initial_node_count = var.node_count
+data "google_client_config" "default" {} #✅ Purpose
+#This is a Terraform data source from the google provider that fetches information about the currently authenticated Google Cloud client used by Terraform. It's especially useful when you need details like:
+#Project,Region/Zone,Access token and Account being used
+#This is useful in dynamic environments where you want Terraform to adapt to the current GCP configuration((for example, based on gcloud auth login).
+
+resource "google_container_cluster" "cluster" {
+  name     = "cluster"
+  location = "us-central1-c"
+
+  release_channel {      #🎯 Purpose
+    channel = "REGULAR"  #release_channel block tells GKE how often your cluster should get Kubernetes and node updates.
+  }                      #Choosing REGULAR helps you stay reasonably up-to-date without risking the instability of very new versions.
+
+  initial_node_count = 2
 
   node_config {
-    machine_type = var.node_machine_type
-    disk_size_gb = 20
+    machine_type = "e2-micro"
+    image_type   = "centos-stream-9" #COS stands for Container-Optimized OS, a lightweight, secure OS maintained by Google, specifically designed for running containers.
+    disk_size_gb = 20 #CONTAINERD indicates that the node will use containerd as the container runtime (instead of the older docker runtime).
   }
-
-  remove_default_node_pool = false
+deletion_protection=false
 }
 
-data "google_client_config" "default" {}
-
-data "google_container_cluster" "primary_creds" {
-  name     = google_container_cluster.primary.name
-  location = google_container_cluster.primary.location
-}
-
-provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.primary_creds.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.primary_creds.master_auth.0.cluster_ca_certificate)
-}
 
 module "delegate" {
   source = "harness/harness-delegate/kubernetes"
@@ -42,21 +49,20 @@ module "delegate" {
   delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.08.86503"
   replicas = 1
   upgrader_enabled = true
-  
-  depends_on = [
-    time_sleep.wait_for_gke_cluster
-  ]
 }
 
-resource "time_sleep" "wait_for_gke_cluster" {
-  create_duration = "120s"
-  depends_on = [google_container_cluster.primary]
+
+provider "kubernetes" { #This Terraform block defines the Kubernetes provider configuration and enables Terraform to interact with your Google Kubernetes Engine (GKE) cluster. 
+  host                   = "https://${google_container_cluster.cluster.endpoint}"
+  cluster_ca_certificate = base64decode(google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
 }
 
-provider "helm" {
+provider "helm" { #It allows Terraform to run Helm commands (like installing charts) against your Kubernetes cluster, by setting up authentication and cluster access.
   kubernetes {
-    host                   = "https://${data.google_container_cluster.primary_creds.endpoint}"
+    host                   = "https://${google_container_cluster.cluster.endpoint}"
+    cluster_ca_certificate = base64decode(google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
     token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(data.google_container_cluster.primary_creds.master_auth.0.cluster_ca_certificate)
   }
 }
+
