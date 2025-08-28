be9304148e40e65b697cec8d17756b1f5429ecdd
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-28 23:07:55 +0530

    Update delegate.tf

--- delegate.tf
+++ delegate.tf
@@ -1,41 +1,21 @@
 # Fetch GKE Cluster Details
-data "google_client_config" "default" {}
-
 data "google_container_cluster" "gke_cluster" {
-  name     = var.cluster_name
-  location = var.region # Change as per your cluster
-  depends_on = [google_container_cluster.primary]
+  name       = google_container_cluster.primary.name
+  location   = var.region
 }
 
-# Kubernetes Provider using GKE authentication
-provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate)
-}
-
-# Helm Provider using GKE authentication
-provider "helm" {
-  kubernetes {
-    host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate)
-  }
-}
-
-
+# Add the Harness delegate module
 module "delegate" {
-  source = "harness/harness-delegate/kubernetes"
-  version = "0.2.3"
-
-  account_id = "ucHySz2jQKKWQweZdXyCog"
-  delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
-  delegate_name = "terraform-delegate-reethu"
-  deploy_mode = "KUBERNETES"
-  namespace = "harness-delegate-ng"
+  source           = "harness/harness-delegate/kubernetes"
+  version          = "0.2.3"
+  account_id       = "ucHySz2jQKKWQweZdXyCog"
+  delegate_token   = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
+  delegate_name    = "terraform-delegate-reethu"
+  deploy_mode      = "KUBERNETES"
+  namespace        = "harness-delegate-ng"
   manager_endpoint = "https://app.harness.io"
-  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.08.86503"
-  replicas = 1
+  delegate_image   = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.08.86503"
+  replicas         = 1
   upgrader_enabled = true
   depends_on       = [google_container_cluster.primary]
 }
