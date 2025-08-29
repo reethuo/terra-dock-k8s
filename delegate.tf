083e92ab4ff65a5e37c8cf9c63d0636781f24138
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-29 13:55:51 +0530

    Update delegate.tf

--- delegate.tf
+++ delegate.tf
@@ -26,17 +26,16 @@ provider "helm" {
 
 module "delegate" {
   source = "harness/harness-delegate/kubernetes"
-  version = "0.1.8"
+  version = "0.2.3"
 
   account_id = "ucHySz2jQKKWQweZdXyCog"
   delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
-  delegate_name = "terraform-delegate"
+  delegate_name = "terraform-delegate-reethu"
   deploy_mode = "KUBERNETES"
   namespace = "harness-delegate-ng"
   manager_endpoint = "https://app.harness.io"
-  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.04.85701"
+  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.08.86503"
   replicas = 1
   upgrader_enabled = true
 }
-
 
