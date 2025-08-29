3234df05a8e567d33cd3a50a1e29e684f14661cc
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-29 13:47:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,15 @@
-# 1️⃣ Create the GKE cluster
 resource "google_container_cluster" "primary" {
-  name                     = var.cluster_name
-  location                 = var.region
-  remove_default_node_pool = true
-  initial_node_count       = 1
+  name     = var.cluster_name
+  location = var.region
 
-  network    = "default"
-  subnetwork = "default"
   deletion_protection = false
-  
-}
 
-# 2️⃣ Create a node pool for the cluster
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "${var.cluster_name}-node-pool"
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
-  node_count = 1
+  initial_node_count = var.node_count
 
   node_config {
     machine_type = var.node_machine_type
-    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    disk_size_gb = 70
   }
+
+  remove_default_node_pool = false
 }
